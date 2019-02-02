1b2ba16ac6edd4cb9a46b483e32bed2fcaba6cc9
AlaricGilbert <alaricgilbert@qq.com>
2019-02-02 22:40:06 +0800

    Changes: 1.We're goting to stop submitting the file changes to this file. 2.Some changes to README.md. New features: \#1 Console (cross-platform, .NET Core) version of this application that can read the tasks.json file and execute the tasks, although it's hard to use. \#2 @ManiaciaChao designed a logo for BC/WPF project. Fixed some bugs in 0.2.0.0

---
+++ BiliCommenter.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using BiliCommenter.Core;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BiliCommenter.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("tasks.json"))
+                return;
+            var tasks = JsonConvert.DeserializeObject<List<CommentTask>>(File.ReadAllText("tasks.json"));
+            foreach (var task in tasks)
+            {
+                task.Callback = new CommentTask.CommentTaskFinishedCallback((taskid) =>
+                {
+                    tasks.Remove(task);
+                    if (tasks.Count == 0)
+                        return;
+                });
+                task.Start();
+            }
+        }
+    }
+}
