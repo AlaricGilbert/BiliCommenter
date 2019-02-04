985222f2c622ce4c96c8d399b07523eebcd6622b
AlaricGilbert <alaricgilbert@qq.com>
2019-02-04 17:08:12 +0800

    feature: inherit settings from previous version.

--- BiliCommenter/Properties/Settings.Designer.cs
+++ BiliCommenter/Properties/Settings.Designer.cs
@@ -58,5 +58,17 @@ namespace BiliCommenter.Properties {
                 this["AccessKey"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool IsUpgrateNeeded {
+            get {
+                return ((bool)(this["IsUpgrateNeeded"]));
+            }
+            set {
+                this["IsUpgrateNeeded"] = value;
+            }
+        }
     }
 }
